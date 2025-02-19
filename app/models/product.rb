e185c9a4f91548505351fd7e677190bddf62f1ca
Hansadhwaja <hansadhwajabiswal@gmail.com>
2025-02-19 18:29:01 +0530

    Display The Catalog

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,10 @@
 class Product < ApplicationRecord
   validates :title, :description, :image_url, presence:true
   validates :title, uniqueness:true
-  validates :image_url,allow_blank:true,format:{
+  validates :image_url, allow_blank:true, format:{
     with: /\.(png|jpg|gif)\z/i,
     message:'must be a URL for GIF,PNG or JPG image'
   }
-  validates :price,numericality:{greater_than_or_equal_to:0.01}
+  validates :price, numericality:{greater_than_or_equal_to: 0.01}
+  validates :title, length:{minimum: 10}
 end
